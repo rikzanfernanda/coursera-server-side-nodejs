80a2dc0195dcb17370b5f5097ccd7c18e0c64d77
rikzan fernanda <fernandarikzan@gmail.com>
2022-08-29 13:05:47 +0700

    chore: delete passport-jwt-app folder

--- passport-jwt-app/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'secretKey': 'qwertyuiop-1234567890',
-    'mongoUrl': 'mongodb://localhost:27017/passport-jwt-app'
-}
--- passport-jwt-app/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'),
-    userController = require('../controllers/userController')
-
-const userRouter = express.Router()
-
-userRouter.post('/signup', userController.store)
-
-module.exports = userRouter
